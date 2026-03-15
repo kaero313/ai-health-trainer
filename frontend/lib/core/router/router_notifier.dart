81458754a47ed8805e836a07b3acbdb414191601
nayatrei <45251952+kaero313@users.noreply.github.com>
2026-03-15 19:53:56 +0900

    feat(router): RouterNotifier에 프로필 상태 변경 감시 추가

--- frontend/lib/core/router/router_notifier.dart
+++ frontend/lib/core/router/router_notifier.dart
@@ -2,12 +2,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../features/auth/domain/auth_state_provider.dart';
+import '../../features/profile/domain/profile_check_provider.dart';
 
 class RouterNotifier extends ChangeNotifier {
   final Ref _ref;
 
   RouterNotifier(this._ref) {
     _ref.listen<AsyncValue<AppAuthState>>(authStateProvider, (_, __) {
+      notifyListeners();
+    });
+    _ref.listen<AsyncValue<bool>>(profileCheckProvider, (_, __) {
       notifyListeners();
     });
   }
